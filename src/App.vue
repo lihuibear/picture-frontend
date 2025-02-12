258ef247df54d8b3057b9c1f9caa87a992e0c0ff
lihuibear <lihuibear4@gmail.com>
2025-02-12 22:16:07 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <BaseLayout />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { healthUsingGet } from '@/api/mainController.ts'
+import BaseLayout from '@/layouts/BaseLayout.vue'
+
+healthUsingGet().then((res) => {
+  console.log(res)
+})
+</script>
---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,80 @@
+<template>
+  <a-row :wrap="false">
+    <a-col flex="200px">
+      <RouterLink to="/">
+        <div class="title-bar">
+          <img class="logo" src="../assets/logo.png" alt="logo" />
+          <div class="title">云图库</div>
+        </div>
+      </RouterLink>
+    </a-col>
+    <a-col flex="auto">
+      <a-menu
+        v-model:selectedKeys="current"
+        mode="horizontal"
+        :items="items"
+        @click="doMenuClick"
+      />
+    </a-col>
+    <a-col flex="120px">
+      <div class="user-login-status">
+        <a-button type="primary" href="/user/login">登录</a-button>
+      </div>
+    </a-col>
+  </a-row>
+</template>
+
+<script lang="ts" setup>
+import { h, ref } from 'vue'
+import { HomeOutlined } from '@ant-design/icons-vue'
+import { MenuProps } from 'ant-design-vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const current = ref<string[]>(['home'])
+// 高亮菜单项
+router.afterEach((to, from, next) => {
+  current.value = [to.path]
+})
+// 路由跳转事件
+const doMenuClick = ({ key }: { key: string }) => {
+  router.push({
+    path: key,
+  })
+}
+const items = ref<MenuProps['items']>([
+  {
+    key: '/',
+    icon: () => h(HomeOutlined),
+    label: '主页',
+    title: '主页',
+  },
+  {
+    key: '/about',
+    label: '关于',
+    title: '关于',
+  },
+  {
+    key: 'others',
+    label: h('a', { href: 'https://lihuibear.cn', target: '_blank' }, 'lihui的主页'),
+    title: 'lihui的主页',
+  },
+])
+</script>
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: black;
+  font-size: 18px;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
