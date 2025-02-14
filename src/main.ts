26ba92c52d181cb08ec2032d2dd8cfc749a96505
lihuibear <lihuibear4@gmail.com>
2025-02-14 15:40:02 +0800

    用户模块(1)

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import router from './router'
 import Antd from 'ant-design-vue'
 import App from './App.vue'
 import 'ant-design-vue/dist/reset.css'
-
+import '@/access'
 const app = createApp(App)
 
 app.use(createPinia())
