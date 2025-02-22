d22dac406d80b95d76833b9590da5e5b33d55bbe
lihuibear <lihuibear4@gmail.com>
2025-02-22 13:51:25 +0800

    添加图片管理及批量创建功能

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,91 +1,68 @@
 <template>
-  <a-row :wrap="false">
-    <a-col flex="200px">
-      <RouterLink to="/">
-        <div class="title-bar">
-          <img class="logo" src="../assets/logo.png" alt="logo" />
-          <div class="title">云图库</div>
-        </div>
-      </RouterLink>
-    </a-col>
-    <a-col flex="auto">
-      <a-menu
-        v-model:selectedKeys="current"
-        mode="horizontal"
-        :items="items"
-        @click="doMenuClick"
-      />
-    </a-col>
-    <a-col flex="120px">
-      <div class="user-login-status">
-        <div v-if="loginUserStore.loginUser.id">
-          <a-dropdown>
-            <ASpace>
-              <a-avatar :src="loginUserStore.loginUser.userAvatar" />
-              {{ loginUserStore.loginUser.userName ?? '无名' }}
-            </ASpace>
-            <template #overlay>
-              <a-menu>
-                <a-menu-item @click="goUserInfoEdit"> 个人中心 </a-menu-item>
-                <a-menu-item @click="doLogout">
-                  <LogoutOutlined />
-                  退出登录
-                </a-menu-item>
-              </a-menu>
-            </template>
-          </a-dropdown>
-        </div>
-        <div v-else>
-          <a-button type="primary" href="/user/login">登录</a-button>
-        </div>
-      </div>
-    </a-col>
-  </a-row>
-</template>
+  <div id="globalHeader">
+    <a-row :wrap="false">
+      <a-col flex="200px">
+        <router-link to="/">
+          <div class="title-bar">
+            <img class="logo" src="../assets/logo.png" alt="logo" />
+            <div class="title">云图库</div>
+          </div>
+        </router-link>
+      </a-col>
+      <a-col flex="auto">
+        <a-menu
+          v-model:selectedKeys="current"
+          mode="horizontal"
+          :items="items"
+          @click="doMenuClick"
+        />
+      </a-col>
+      <!-- 用户信息展示栏 -->
+      <a-col flex="120px">
+        <div class="user-login-status">
+          <div v-if="loginUserStore.loginUser.id">
+            <a-dropdown>
+              <a-space>
+                <a-avatar :src="loginUserStore.loginUser.userAvatar" />
+                {{ loginUserStore.loginUser.userName ?? '无名' }}
+              </a-space>
+              <template #overlay>
+                <a-menu>
+                  <a-menu-item @click="goUserInfoEdit">
+                    <UserOutlined />
+                    个人中心
+                  </a-menu-item>
+                  <a-menu-item @click="doLogout">
+                    <LogoutOutlined />
 
+                    退出登录
+                  </a-menu-item>
+                </a-menu>
+              </template>
+            </a-dropdown>
+          </div>
+          <div v-else>
+            <a-button type="primary" href="/user/login">登录</a-button>
+          </div>
+        </div>
+      </a-col>
+    </a-row>
+  </div>
+</template>
 <script lang="ts" setup>
 import { computed, h, ref } from 'vue'
-import { HomeOutlined } from '@ant-design/icons-vue'
+import { HomeOutlined, LogoutOutlined, UserOutlined } from '@ant-design/icons-vue'
 import { MenuProps, message } from 'ant-design-vue'
 import { useRouter } from 'vue-router'
 import { useLoginUserStore } from '@/stores/useLoginUserStore.ts'
 import { userLogoutUsingPost } from '@/api/userController.ts'
 
 const loginUserStore = useLoginUserStore()
-const router = useRouter()
-
-const current = ref<string[]>(['home'])
-// 高亮菜单项
-router.afterEach((to, from, next) => {
-  current.value = [to.path]
-})
-// 路由跳转事件
-const doMenuClick = ({ key }: { key: string }) => {
-  router.push({
-    path: key,
-  })
-}
-// 用户注销
-const doLogout = async () => {
-  const res = await userLogoutUsingPost()
-  console.log(res)
-  if (res.data.code === 0) {
-    loginUserStore.setLoginUser({
-      userName: '未登录',
-    })
-    message.success('退出登录成功')
-    await router.push('/user/login')
-  } else {
-    message.error('退出登录失败，' + res.data.message)
-  }
-}
-
 // 跳转到用户编辑页面
 const goUserInfoEdit = () => {
   router.push('/user/userinfo')
 }
-
-// 菜单列表
+// 未经过滤的菜单项
 const originItems = [
   {
     key: '/',
@@ -94,21 +71,32 @@ const originItems = [
     title: '主页',
   },
   {
+    key: '/add_picture',
+    label: '创建图片',
+    title: '创建图片',
+  },
+  {
     key: '/admin/userManage',
     label: '用户管理',
     title: '用户管理',
   },
   {
+    key: '/admin/pictureManage',
+    label: '图片管理',
+    title: '图片管理',
+  },
+  {
     key: 'others',
-    label: h('a', { href: 'https://lihuibear.cn', target: '_blank' }, 'lihui的主页'),
-    title: 'lihui的主页',
+    label: h('a', { href: 'https://lihuibear.cn', target: '_blank' }, '个人主页'),
+    title: '个人主页',
   },
 ]
 
-// 过滤菜单项
+// 根据权限过滤菜单项
 const filterMenus = (menus = [] as MenuProps['items']) => {
   return menus?.filter((menu) => {
-    if (menu.key.startsWith('/admin')) {
+    // 管理员才能看到 /admin 开头的菜单
+    if (menu?.key?.startsWith('/admin')) {
       const loginUser = loginUserStore.loginUser
       if (!loginUser || loginUser.userRole !== 'admin') {
         return false
@@ -119,10 +107,40 @@ const filterMenus = (menus = [] as MenuProps['items']) => {
 }
 
 // 展示在菜单的路由数组
-const items = computed<MenuProps['items']>(() => filterMenus(originItems))
+const items = computed(() => filterMenus(originItems))
+
+const router = useRouter()
+// 当前要高亮的菜单项
+const current = ref<string[]>([])
+// 监听路由变化，更新高亮菜单项
+router.afterEach((to, from, next) => {
+  current.value = [to.path]
+})
+
+// 路由跳转事件
+const doMenuClick = ({ key }) => {
+  router.push({
+    path: key,
+  })
+}
+
+// 用户注销
+const doLogout = async () => {
+  const res = await userLogoutUsingPost()
+  if (res.data.code === 0) {
+    loginUserStore.setLoginUser({
+      userName: '未登录',
+    })
+    message.success('退出登录成功')
+    await router.push('/user/login')
+  } else {
+    message.error('退出登录失败，' + res.data.message)
+  }
+}
 </script>
+
 <style scoped>
-.title-bar {
+#globalHeader .title-bar {
   display: flex;
   align-items: center;
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,14 +3,18 @@ import HomePage from '@/page/HomePage.vue'
 import UserLoginPage from '@/page/user/UserLoginPage.vue'
 import UserRegisterPage from '@/page/user/UserRegisterPage.vue'
 import UserManagePage from '@/page/admin/UserManagePage.vue'
+import AddPicturePage from '@/page/AddPicturePage.vue'
+import PictureDetailPage from '@/page/PictureDetailPage.vue'
+import PictureManagePage from '@/page/admin/PictureManagePage.vue'
 import UserInfoEdit from '@/page/user/UserInfoEdit.vue'
+import AddPictureBatchPage from '@/page/AddPictureBatchPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: '主页',
+      name: 'home',
       component: HomePage,
     },
     {
@@ -23,15 +27,43 @@ const router = createRouter({
       name: '用户注册',
       component: UserRegisterPage,
     },
-
     {
       path: '/admin/userManage',
       name: '用户管理',
       component: UserManagePage,
     },
     {
+      path: '/admin/pictureManage',
+      name: '图片管理',
+      component: PictureManagePage,
+    },
+    {
+      path: '/add_picture',
+      name: '创建图片',
+      component: AddPicturePage,
+    },
+    {
+      path: '/picture/:id',
+      name: '图片详情',
+      component: PictureDetailPage,
+      props: true,
+    },
+    {
+      path: '/add_picture/batch',
+      name: '批量创建图片',
+      component: AddPictureBatchPage,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
       path: '/user/userinfo',
-      name: '个人中心',
+      name: '个人信息',
       component: UserInfoEdit,
     },
   ],
